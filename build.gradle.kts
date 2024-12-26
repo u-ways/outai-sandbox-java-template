43027564f3e723911866afc0ab97c402dcc8bc42
u-ways <u.ways.mail@gmail.com>
2024-12-26 16:49:07 +0000

    refactor: unless required let's disable the unused dependencies except the common ones as Sphere IDE is quite slow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,29 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+//import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    alias(libs.plugins.spotless)
-    alias(libs.plugins.sonarlint)
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+//    alias(libs.plugins.spotless)
+//    alias(libs.plugins.sonarlint)
+//    alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 group = "outai.sandbox"
 version = "DEV-SNAPSHOT"
 
 dependencies {
-    implementation(libs.kotlin.reflect)
+//    implementation(libs.kotlin.reflect)
 
-    testImplementation(libs.mockk.core)
-    testImplementation(libs.kotest.runner.junit5)
-    testImplementation(libs.kotest.assertions.core)
+//    testImplementation(libs.mockk.core)
+//    testImplementation(libs.kotest.runner.junit5)
+//    testImplementation(libs.kotest.assertions.core)
 
     testImplementation(libs.junit.jupiter)
-    testImplementation(libs.wiremock)
     testImplementation(libs.mockito.junit.jupiter)
-    testImplementation(libs.rest.assured)
     testImplementation(libs.mockito.core)
     testImplementation(libs.hamcrest)
+//    testImplementation(libs.wiremock)
+//    testImplementation(libs.rest.assured)
 
     testRuntimeOnly(libs.junit.platform.launcher)
 }
@@ -34,12 +34,12 @@ java {
 }
 
 tasks {
-    withType<KotlinCompile> {
-        compilerOptions {
-            freeCompilerArgs.add("-Xjsr305=strict")
-            jvmTarget.set(JvmTarget.JVM_18)
-        }
-    }
+//    withType<KotlinCompile> {
+//        compilerOptions {
+//            freeCompilerArgs.add("-Xjsr305=strict")
+//            jvmTarget.set(JvmTarget.JVM_18)
+//        }
+//    }
 
     withType<Test> {
         useJUnitPlatform()
@@ -66,33 +66,33 @@ tasks {
     }
 }
 
-spotless {
-    java {
-        importOrder()
-        removeUnusedImports()
-        palantirJavaFormat()
-    }
-}
-
-sonarlint {
-    maxIssues.set(0)
-    showIssues.set(true)
-    ignoreFailures.set(false)
-    reports { create("html") { enabled.set(true) } }
-    /**
-     * Rules are described here:
-     * - https://rules.sonarsource.com/java/
-     *
-     * You can quickly search for a rule by its key, e.g. for `java:S1186`:
-     * - https://rules.sonarsource.com/java/RSPEC-1186
-     *
-     * Note that some rules are for SonarQube or SonarCloud only.
-     *
-     * In a real-world project, you would want to enable more rules or
-     * create more tailored profiles for your project.
-     */
-    includeRules.addAll(
-        "java:S1186",
-        "java:S1190",
-    )
-}
+//spotless {
+//    java {
+//        importOrder()
+//        removeUnusedImports()
+//        palantirJavaFormat()
+//    }
+//}
+//
+//sonarlint {
+//    maxIssues.set(0)
+//    showIssues.set(true)
+//    ignoreFailures.set(false)
+//    reports { create("html") { enabled.set(true) } }
+//    /**
+//     * Rules are described here:
+//     * - https://rules.sonarsource.com/java/
+//     *
+//     * You can quickly search for a rule by its key, e.g. for `java:S1186`:
+//     * - https://rules.sonarsource.com/java/RSPEC-1186
+//     *
+//     * Note that some rules are for SonarQube or SonarCloud only.
+//     *
+//     * In a real-world project, you would want to enable more rules or
+//     * create more tailored profiles for your project.
+//     */
+//    includeRules.addAll(
+//        "java:S1186",
+//        "java:S1190",
+//    )
+//}
